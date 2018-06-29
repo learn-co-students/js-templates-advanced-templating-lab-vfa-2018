07867ad04700718ded59ce466e78cff8454edf0a
Michael Lee <leemichael289@gmail.com>
2018-06-29 04:11:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function init() {
-"I'm so lost"
+   //put any page initialization/handlebars initialization here
+ }
+
+ document.addEventListener("DOMContentLoaded", function(event) {
+   init()
+ })
+
+function createRecipe() {
+  var recipe = {
+  }
 }
-document.addEventListener("DOMContentLoaded", function(event) {
-  init()
-})
